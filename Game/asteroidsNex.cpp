fb0ce00abce47936b7ee6add0f6a32df57f06117
Aldo <aldomanuel2006@gmail.com>
2025-06-23 00:29:42 -0600

    ScoreOvni

--- Game/asteroidsNex.cpp
+++ Game/asteroidsNex.cpp
@@ -1058,7 +1058,6 @@ int main() {
                             score = 0;
                         }
                     }
-
                     break;
                 }
             }//for (ovni)
